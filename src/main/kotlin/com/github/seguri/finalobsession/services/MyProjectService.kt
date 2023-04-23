974622632f55967a04e92f85fd4c28590da34b55
Marco Seguri <seguri@users.noreply.github.com>
2023-04-24 00:47:41 +0200

    Spotless

--- src/main/kotlin/com/github/seguri/finalobsession/services/MyProjectService.kt
+++ src/main/kotlin/com/github/seguri/finalobsession/services/MyProjectService.kt
@@ -1,17 +1,19 @@
 package com.github.seguri.finalobsession.services
 
+import com.github.seguri.finalobsession.MyBundle
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import com.github.seguri.finalobsession.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
 
-    init {
-        thisLogger().info(MyBundle.message("projectService", project.name))
-        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
-    }
+  init {
+    thisLogger().info(MyBundle.message("projectService", project.name))
+    thisLogger()
+        .warn(
+            "Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
+  }
 
-    fun getRandomNumber() = (1..100).random()
+  fun getRandomNumber() = (1..100).random()
 }
--- src/main/kotlin/com/github/seguri/finalobsession/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/seguri/finalobsession/toolWindow/MyToolWindowFactory.kt
@@ -1,5 +1,7 @@
 package com.github.seguri.finalobsession.toolWindow
 
+import com.github.seguri.finalobsession.MyBundle
+import com.github.seguri.finalobsession.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
@@ -8,40 +10,41 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import com.github.seguri.finalobsession.MyBundle
-import com.github.seguri.finalobsession.services.MyProjectService
 import javax.swing.JButton
-
 
 class MyToolWindowFactory : ToolWindowFactory {
 
-    init {
-        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
-    }
+  init {
+    thisLogger()
+        .warn(
+            "Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
+  }
 
-    private val contentFactory = ContentFactory.SERVICE.getInstance()
+  private val contentFactory = ContentFactory.SERVICE.getInstance()
 
-    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        val myToolWindow = MyToolWindow(toolWindow)
-        val content = contentFactory.createContent(myToolWindow.getContent(), null, false)
-        toolWindow.contentManager.addContent(content)
-    }
+  override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+    val myToolWindow = MyToolWindow(toolWindow)
+    val content = contentFactory.createContent(myToolWindow.getContent(), null, false)
+    toolWindow.contentManager.addContent(content)
+  }
 
-    override fun shouldBeAvailable(project: Project) = true
+  override fun shouldBeAvailable(project: Project) = true
 
-    class MyToolWindow(toolWindow: ToolWindow) {
+  class MyToolWindow(toolWindow: ToolWindow) {
 
-        private val service = toolWindow.project.service<MyProjectService>()
+    private val service = toolWindow.project.service<MyProjectService>()
 
-        fun getContent() = JBPanel<JBPanel<*>>().apply {
-            val label = JBLabel(MyBundle.message("randomLabel", "?"))
+    fun getContent() =
+        JBPanel<JBPanel<*>>().apply {
+          val label = JBLabel(MyBundle.message("randomLabel", "?"))
 
-            add(label)
-            add(JButton(MyBundle.message("shuffle")).apply {
+          add(label)
+          add(
+              JButton(MyBundle.message("shuffle")).apply {
                 addActionListener {
-                    label.text = MyBundle.message("randomLabel", service.getRandomNumber())
+                  label.text = MyBundle.message("randomLabel", service.getRandomNumber())
                 }
-            })
+              })
         }
-    }
+  }
 }
